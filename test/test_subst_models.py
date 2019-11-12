3dcc1f8fa0492f7ec8aa055f7a2ad533321deb72
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-11-13 09:24:07 +1300

    Frequency differential test and fix bugs

--- test/test_subst_models.py
+++ test/test_subst_models.py
@@ -15,3 +15,18 @@ def test_hky_eigen(hky_params):
     assert_eigen(HKY(), **hky_params)
 
 # TODO: test_gtr_eigen
+
+def assert_freq_differentials(subst_model, frequencies, **params):
+    with tf.GradientTape() as t:
+        t.watch(frequencies)
+        q = subst_model.q(frequencies, **params)
+    tf_jac = t.jacobian(q, frequencies)
+    tf_diffs = tf.transpose(tf_jac, perm=[2, 0, 1]).numpy()
+    diffs = subst_model.q_frequency_differentials(frequencies, **params)
+    print(diffs)
+    print(tf_diffs)
+    assert_allclose(tf_diffs, diffs)
+
+def test_hky_frequency_differentials(hky_params):
+    assert_freq_differentials(HKY(), **hky_params)
+
