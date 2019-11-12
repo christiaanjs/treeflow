c6d77776f04a9c9907ce9440c1e0bc292a301dae
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-11-12 21:10:55 +1300

    Refactor substitution models into classes

--- treeflow/substitution_model.py
+++ treeflow/substitution_model.py
@@ -1,106 +1,138 @@
 import tensorflow as tf
 import numpy as np
 
-jc_eigendecomposition = [np.array(x) for x in [
-    [
-        [1.0, 2.0, 0.0, 0.5],
-        [1.0, -2.0, 0.5, 0.0],
-        [1.0, 2.0, 0.0, -0.5],
-        [1.0, -2.0, -0.5, 0.0]
-    ],
-    [0.0, -1.3333333333333333, -1.3333333333333333, -1.3333333333333333],
-    [
-        [0.25, 0.25, 0.25, 0.25],
-        [0.125, -0.125, 0.125, -0.125],
-        [0.0, 1.0, 0.0, -1.0],
-        [1.0, 0.0, -1.0, 0.0]
-    ]
-]]
-
-jc_frequencies = np.array([0.25, 0.25, 0.25, 0.25])
-
 A, C, G, T = list(range(4))
 
-def hky_eigendecomposition(pi, kappa):
-	piY = pi[T] + pi[C]
-	piR = pi[A] + pi[G]
+class SubstitutionModel:
+    def q(frequencies, **kwargs):
+        raise NotImplementedError()
 
-	beta = -1.0 / (2.0 * (piR*piY + kappa * (pi[A]*pi[G] + pi[C]*pi[T])))
-	A_R = 1.0 + piR * (kappa - 1)
-	A_Y = 1.0 + piY * (kappa - 1)
-	lambd = tf.stack([ # Eigenvalues 
-		0.0,
-		beta,
-		beta * A_Y,
-		beta * A_R
-	])
-	U = tf.transpose(tf.stack([ # Right eigenvectors as columns (rows of transpose)
-		[1.0, 1.0, 1.0, 1.0],
-		[1.0/piR, -1.0/piY, 1.0/piR, -1.0/piY],
-		[0.0, pi[T]/piY, 0.0, -pi[C]/piY],
-		[pi[G]/piR, 0.0, -pi[A]/piR, 0.0]
-	]))
+    def eigen(frequencies, **kwargs):
+        raise NotImplementedError()
 
-	Vt = tf.stack([ # Left eigenvectors as rows
-		[pi[A], pi[C], pi[G], pi[T]],
-		[pi[A]*piY, -pi[C]*piR, pi[G]*piY, -pi[T]*piR],
-		[0.0, 1.0, 0.0, -1.0],
-		[1.0, 0.0, -1.0, 0.0]
-	])
+    def q_frequency_differentials(frequencies, **kwargs):
+        raise NotImplementedError()
 
-	return [tf.dtypes.cast(x, tf.dtypes.float64) for x in [U, lambd, Vt]]
+    def q_param_differentials(frequencies, **kwargs):
+        raise NotImplementedError()
 
-def hky_q_matrix(pi, kappa):
-    return tf.stack([
-        [-(pi[C] + kappa*pi[G] + pi[T]), pi[C], kappa*pi[G], pi[T]],
-        [pi[A], -(pi[A] + pi[G] + kappa*pi[T]) , pi[G], kappa*pi[T]],
-        [kappa*pi[A], pi[C], -(kappa*pi[A] + pi[C] + pi[T]), pi[T]],
-        [pi[A], kappa*pi[C], pi[G], -(pi[A] + kappa*pi[C] + pi[G])]
-    ])
+    def param_keys():
+        raise NotImplementedError()
 
-def hky_kappa_differential(pi):
-    return tf.stack([
-        [-pi[G], 0.0, pi[G], 0.0],
-        [0.0, -pi[T], 0.0, pi[T]],
-        [pi[A], 0.0, -pi[A], 0.0],
-        [0.0, pi[C], 0.0, -pi[C]]
-    ])
+class JC(SubstitutionModel):
+    def q(*args, **kwargs):
+        return tf.convert_to_tensor(np.array([0.25, 0.25, 0.25, 0.25]))
 
-def hky_frequencies_differential(kappa):
-    return tf.stack([
-        [
-            [0.0, 0.0, 0.0, 0.0],
-            [1.0, -1.0, 0.0, 0.0],
-            [kappa, 0.0, -kappa, 0.0],
+    def eigen(*args, **kwargs):
+        return [tf.convert_to_tensor(np.array(x)) for x in [
+            [
+                [1.0, 2.0, 0.0, 0.5],
+                [1.0, -2.0, 0.5, 0.0],
+                [1.0, 2.0, 0.0, -0.5],
+                [1.0, -2.0, -0.5, 0.0]
+            ],
+            [0.0, -1.3333333333333333, -1.3333333333333333, -1.3333333333333333],
+            [
+                [0.25, 0.25, 0.25, 0.25],
+                [0.125, -0.125, 0.125, -0.125],
+                [0.0, 1.0, 0.0, -1.0],
+                [1.0, 0.0, -1.0, 0.0]
+            ]
+        ]]
+
+
+class HKY(SubstitutionModel):
+    def eigen(frequencies, kappa):
+        pi = frequencies
+        piY = pi[T] + pi[C]
+        piR = pi[A] + pi[G]
+
+        beta = -1.0 / (2.0 * (piR*piY + kappa * (pi[A]*pi[G] + pi[C]*pi[T])))
+        A_R = 1.0 + piR * (kappa - 1)
+        A_Y = 1.0 + piY * (kappa - 1)
+        eval = tf.stack([ # Eigenvalues 
+            0.0,
+            beta,
+            beta * A_Y,
+            beta * A_R
+        ])
+        evec = tf.transpose(tf.stack([ # Right eigenvectors as columns (rows of transpose)
+            [1.0, 1.0, 1.0, 1.0],
+            [1.0/piR, -1.0/piY, 1.0/piR, -1.0/piY],
+            [0.0, pi[T]/piY, 0.0, -pi[C]/piY],
+            [pi[G]/piR, 0.0, -pi[A]/piR, 0.0]
+        ]))
+
+        ivec = tf.stack([ # Left eigenvectors as rows
+            [pi[A], pi[C], pi[G], pi[T]],
+            [pi[A]*piY, -pi[C]*piR, pi[G]*piY, -pi[T]*piR],
+            [0.0, 1.0, 0.0, -1.0],
             [1.0, 0.0, -1.0, 0.0]
-        ],
-        [
-            [-1.0, 1.0, 0.0, 0.0],
-            [0.0, 0.0, 0.0, 0.0],
-            [0.0, 1.0, -1.0, 0.0],
-            [0.0, kappa, 0.0, -kappa]
-        ],
-        [
-            [-kappa, 0.0, kappa, 0.0],
-            [0.0, -1.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 0.0],
-            [0.0, 0.0, 1.0, -1.0]
-        ],
-        [
-            [-1.0, 0.0, 0.0, 1.0],
-            [0.0, -kappa, 0.0, kappa],
-            [0.0, 0.0, -1.0, 0.0],
-            [0.0, 0.0, 0.0, 0.0]
-        ]
-    ])
+        ])
 
-def gtr_q_matrix(pi, rates):
-    return tf.stack([
-        [-(rates[0]*pi[1] + rates[1]*pi[2] + rates[2]*pi[3]), rates[0]*pi[1], rates[1]*pi[2], rates[2]*pi[3]],
-        [rates[0]*pi[0], -(rates[0]*pi[0] + rates[3]*pi[2] + rates[4]*pi[3]), rates[3]*pi[2], rates[4]*pi[3]],
-        [rates[1]*pi[0], rates[3]*pi[1], -(rates[1]*pi[0] + rates[3]*pi[1] + rates[5]*pi[3]), rates[5]*pi[3]],
-        [rates[2]*pi[0], rates[4]*pi[1], rates[5]*pi[2], -(rates[2]*pi[0] + rates[4]*pi[1] + rates[5]*pi[2])]     
-    ])
+        return [tf.dtypes.cast(x, tf.dtypes.float64) for x in [evec, eval, ivec]]
+
+    def q(frequencies, kappa):
+        pi = frequencies
+        return tf.stack([
+            [-(pi[C] + kappa*pi[G] + pi[T]), pi[C], kappa*pi[G], pi[T]],
+            [pi[A], -(pi[A] + pi[G] + kappa*pi[T]) , pi[G], kappa*pi[T]],
+            [kappa*pi[A], pi[C], -(kappa*pi[A] + pi[C] + pi[T]), pi[T]],
+            [pi[A], kappa*pi[C], pi[G], -(pi[A] + kappa*pi[C] + pi[G])]
+        ])
+
+    def q_param_differentials(frequencies, kappa):
+        pi = frequencies
+        return tf.stack([
+            [-pi[G], 0.0, pi[G], 0.0],
+            [0.0, -pi[T], 0.0, pi[T]],
+            [pi[A], 0.0, -pi[A], 0.0],
+            [0.0, pi[C], 0.0, -pi[C]]
+        ])
+
+    def q_frequency_differentials(frequencies, kappa):
+        return tf.stack([
+            [
+                [0.0, 0.0, 0.0, 0.0],
+                [1.0, -1.0, 0.0, 0.0],
+                [kappa, 0.0, -kappa, 0.0],
+                [1.0, 0.0, -1.0, 0.0]
+            ],
+            [
+                [-1.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, -1.0, 0.0],
+                [0.0, kappa, 0.0, -kappa]
+            ],
+            [
+                [-kappa, 0.0, kappa, 0.0],
+                [0.0, -1.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, -1.0]
+            ],
+            [
+                [-1.0, 0.0, 0.0, 1.0],
+                [0.0, -kappa, 0.0, kappa],
+                [0.0, 0.0, -1.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0]
+            ]
+        ])
+
+class GTR(SubstitutionModel):
+    def q(frequencies, rates):
+        pi = frequencies
+        return tf.stack([
+            [-(rates[0]*pi[1] + rates[1]*pi[2] + rates[2]*pi[3]), rates[0]*pi[1], rates[1]*pi[2], rates[2]*pi[3]],
+            [rates[0]*pi[0], -(rates[0]*pi[0] + rates[3]*pi[2] + rates[4]*pi[3]), rates[3]*pi[2], rates[4]*pi[3]],
+            [rates[1]*pi[0], rates[3]*pi[1], -(rates[1]*pi[0] + rates[3]*pi[1] + rates[5]*pi[3]), rates[5]*pi[3]],
+            [rates[2]*pi[0], rates[4]*pi[1], rates[5]*pi[2], -(rates[2]*pi[0] + rates[4]*pi[1] + rates[5]*pi[2])]     
+        ])
+
+    def eigen(frequencies, rates):
+        q = q(frequencies, rates)
+        eval, evec = tf.linalg.eigh(q)
+        ivec = tf.linalg.inv(evec)
+        return (evec, eval, ivec)
 
 def normalising_constant(q, pi):
     return -tf.reduce_sum(tf.linalg.diag_part(q) * pi)
