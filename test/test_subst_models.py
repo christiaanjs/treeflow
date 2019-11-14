97da2890de7ee47d5c37cef2b22333a4d87136fd
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-11-14 14:45:00 +1300

    Tests for param differentials

--- test/test_subst_models.py
+++ test/test_subst_models.py
@@ -1,3 +1,4 @@
+import pytest
 import tensorflow as tf
 from numpy.testing import assert_allclose
 from treeflow.substitution_model import JC, HKY
@@ -23,10 +24,24 @@ def assert_freq_differentials(subst_model, frequencies, **params):
     tf_jac = t.jacobian(q, frequencies)
     tf_diffs = tf.transpose(tf_jac, perm=[2, 0, 1]).numpy()
     diffs = subst_model.q_frequency_differentials(frequencies, **params)
-    print(diffs)
-    print(tf_diffs)
     assert_allclose(tf_diffs, diffs)
 
 def test_hky_frequency_differentials(hky_params):
     assert_freq_differentials(HKY(), **hky_params)
 
+def assert_param_differentials(subst_model, param_key, **params):
+    with tf.GradientTape() as t:
+        t.watch(params[param_key])
+        q = subst_model.q(**params)
+    tf_jac = t.jacobian(q, params[param_key])
+    diffs = subst_model.q_param_differentials(**params)[param_key]
+    if tf.rank(diffs) > 2:
+        tf_diffs = tf.transpose(tf_jac, perm=[2, 0, 1]).numpy()
+    else:
+        tf_diffs = tf_jac.numpy()
+
+    assert_allclose(tf_diffs, diffs)
+        
+@pytest.mark.parametrize("param_key", HKY().param_keys())
+def test_hky_param_differentials(hky_params, param_key):
+    assert_param_differentials(HKY(), param_key, **hky_params)
