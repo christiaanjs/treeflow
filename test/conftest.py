Test all site model values, fix branch gradient

test/conftest.py
@@ -27,15 +27,6 @@ def hky_params(request):
 def freq_index(request):
     return request.param
 
-
-@pytest.fixture
-def branch_lengths():
-    return np.array([0.4, 2.2])
-
-@pytest.fixture
-def category_rates():
-    return np.array([1.1, 0.9])
-
 data_dir = Path('data')
 
 @pytest.fixture
@@ -46,11 +37,28 @@ def hello_newick_file():
 def hello_fasta_file():
     return str(data_dir / 'hello.fasta')
 
+@pytest.fixture(params=[tf.convert_to_tensor(np.array([0.1])), tf.convert_to_tensor(np.array([0.4, 2.2]))])
+def branch_lengths(request):
+    return request.param
+
+def single_rates_():
+    return tf.convert_to_tensor(np.array([1.0]))
 
 @pytest.fixture
 def single_rates():
+    return single_rates_()
+
+@pytest.fixture(params=[single_rates_(), tf.convert_to_tensor(np.array([0.9, 1.1]))])
+def category_rates(request):
+    return request.param
+
+def single_weights_():
     return tf.convert_to_tensor(np.array([1.0]))
 
 @pytest.fixture
 def single_weights():
-    return tf.convert_to_tensor(np.array([1.0]))
+    return single_weights_()
+
+@pytest.fixture(params=[single_weights_(), tf.convert_to_tensor(np.array([0.6, 0.4]))])
+def category_weights(request):
+    return request.param

test/test_subst_model_util.py
@@ -54,7 +54,7 @@ def test_normalisation_gradient(hky_params, param_key):
     assert_allclose(tf_diffs, norm_diffs)
 
 @pytest.mark.parametrize('inv_mult', [False, True])
-def test_transition_prob_differential_tf_hky_kappa(hky_params, single_rates, inv_mult):
+def test_transition_prob_differential_tf_hky_kappa(hky_params, category_rates, inv_mult):
     param_key = 'kappa'
     branch_lengths = tf.convert_to_tensor(np.array([0.9, 2.2]))
     subst_model = HKY()
@@ -62,20 +62,20 @@ def test_transition_prob_differential_tf_hky_kappa(hky_params, single_rates, inv
     with tf.GradientTape(persistent=True) as t:
         t.watch(params[param_key])
         q_norm = subst_model.q_norm(**params)
-        transition_probs_vals = transition_probs_expm(q_norm, single_rates, branch_lengths)
+        transition_probs_vals = transition_probs_expm(q_norm, category_rates, branch_lengths)
     tf_jacs = t.jacobian(transition_probs_vals, params[param_key], experimental_use_pfor=False) 
     transition_probs_inv = tf.linalg.inv(transition_probs_vals)
     
     eigen = subst_model.eigen(**params)
 
     q_diffs = subst_model.q_norm_param_differentials(**params)[param_key]
-    diffs = transition_probs_differential(q_diffs, eigen, branch_lengths, single_rates, inv_mult=inv_mult)
+    diffs = transition_probs_differential(q_diffs, eigen, branch_lengths, category_rates, inv_mult=inv_mult)
     tf_diffs = (transition_probs_inv @ tf_jacs) if inv_mult else tf_jacs
     
     assert_allclose(tf_diffs.numpy(), diffs.numpy())
 
 @pytest.mark.parametrize('inv_mult', [False, True])
-def test_transition_prob_differential_tf_hky_frequencies(hky_params, single_rates, inv_mult, freq_index):
+def test_transition_prob_differential_tf_hky_frequencies(hky_params, category_rates, inv_mult, freq_index):
     branch_lengths = tf.convert_to_tensor(np.array([0.9, 2.2]))
     subst_model = HKY()
     params = hky_params
@@ -83,14 +83,14 @@ def test_transition_prob_differential_tf_hky_frequencies(hky_params, single_rate
     with tf.GradientTape(persistent=True) as t:
         t.watch(params[param_key])
         q_norm = subst_model.q_norm(**params)
-        transition_probs_vals = transition_probs_expm(q_norm, single_rates, branch_lengths)
+        transition_probs_vals = transition_probs_expm(q_norm, category_rates, branch_lengths)
     tf_jacs = t.jacobian(transition_probs_vals, params[param_key], experimental_use_pfor=False) 
     transition_probs_inv = tf.linalg.inv(transition_probs_vals)
     
     eigen = subst_model.eigen(**params)
     
     q_diffs = subst_model.q_norm_frequency_differentials(**params)[freq_index]
-    diffs = transition_probs_differential(q_diffs, eigen, branch_lengths, single_rates, inv_mult=inv_mult)
+    diffs = transition_probs_differential(q_diffs, eigen, branch_lengths, category_rates, inv_mult=inv_mult)
     tf_diffs = (transition_probs_inv @ tf_jacs[:, :, :, :, freq_index]) if inv_mult else tf_jacs[:, :, :, :, freq_index] 
 
     assert_allclose(tf_diffs.numpy(), diffs.numpy())

treeflow/tensorflow_likelihood.py
@@ -145,8 +145,11 @@ class TensorflowLikelihood(BaseLikelihood):
         site_derivatives = self.compute_site_derivatives(differential_matrices, category_weights)
         return tf.reduce_sum(self.pattern_counts / site_likelihoods * site_derivatives, axis=-1)
 
-    def compute_branch_length_derivatives(self, q, category_weights):
-        return self.compute_edge_derivatives(tf.reshape(q, [1, 1, 4, 4]), category_weights)
+    def compute_branch_length_derivatives(self, q, category_rates, category_weights):
+        site_coefficients = self.pattern_counts / self.compute_site_likelihoods(category_weights)
+        cat_derivatives = self.compute_cat_derivatives(tf.reshape(q, [1, 1, 4, 4])) * tf.reshape(category_rates, [1, -1])
+        site_derivatives = tf.reduce_sum(cat_derivatives * category_weights, axis=-1)
+        return tf.reduce_sum(site_coefficients * site_derivatives, axis=-1) 
 
     def compute_frequency_derivative(self, differential_matrices, frequency_index, category_weights):
         site_likelihoods = self.compute_site_likelihoods(category_weights)