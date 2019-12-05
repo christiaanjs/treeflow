593733220d9ab42eaf913dbf85d7d93d77c9687b
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2019-12-05 14:41:43 +1300

    Refactor coalescent into structured variable

--- test/test_coalescent.py
+++ test/test_coalescent.py
@@ -7,10 +7,11 @@ from treeflow.coalescent import ConstantCoalescent
 
 def test_coalescent_homochronous():
     pop_size = tf.convert_to_tensor(np.array(10000.0))
-    heights = tf.convert_to_tensor(np.array([0.0, 0.0, 0.0, 1.0, 2.0]))
-    node_mask = tf.convert_to_tensor(np.array([False, False, False, True, True]))
-    dist = ConstantCoalescent(pop_size, node_mask)
-    res = dist.log_prob(heights)
+    sampling_times = tf.convert_to_tensor(np.array([0.0, 0.0, 0.0]))
+    heights = tf.convert_to_tensor(np.array([1.0, 2.0]))
+    parent_indices = tf.convert_to_tensor(np.array([3, 3, 4, 4]))
+    dist = ConstantCoalescent(pop_size, sampling_times)
+    res = dist.log_prob(dict(heights=heights, topology=dict(parent_indices=parent_indices)))
     expected =  -(4 / pop_size) - 2 * np.log(pop_size)
     assert_allclose(res.numpy(), expected)
 
@@ -18,8 +19,9 @@ test_data = [(123.0,-14.446309163678226),(999.0,-20.721465537146862)]
 @pytest.mark.parametrize('pop_size,expected', test_data)
 def test_coalescent_heterochronous(pop_size, expected):
     pop_size = tf.convert_to_tensor(np.array(pop_size))
-    heights = tf.convert_to_tensor(np.array([0.0, 0.1, 0.4, 0.0, 0.2, 0.5, 0.8]))
-    node_mask = tf.convert_to_tensor(np.array([False, False, False, False, True, True, True]))
-    dist = ConstantCoalescent(pop_size, node_mask)
-    res = dist.log_prob(heights)
+    sampling_times = tf.convert_to_tensor(np.array([0.0, 0.1, 0.4, 0.0]))
+    heights = tf.convert_to_tensor(np.array([0.2, 0.5, 0.8]))
+    parent_indices = tf.convert_to_tensor(np.array([4, 4, 5, 6, 5, 6]))
+    dist = ConstantCoalescent(pop_size, sampling_times)
+    res = dist.log_prob(dict(heights=heights, topology=dict(parent_indices=parent_indices)))
     assert_allclose(res.numpy(), expected)
--- treeflow/coalescent.py
+++ treeflow/coalescent.py
@@ -14,30 +14,44 @@ def get_lineage_count(event_types):
     return tf.math.cumsum(event_types)
 
 class ConstantCoalescent(tfp.distributions.Distribution):
-    def __init__(self, pop_size,node_mask,
+    def __init__(self, pop_size,sampling_times,
                validate_args=False,
                allow_nan_stats=True,
                name='ConstantCoalescent'):
         super(ConstantCoalescent, self).__init__(
-            dtype=None,
+            dtype={
+                'heights': sampling_times.dtype,
+                'topology': {
+                    'parent_indices': tf.int64
+                }
+            },
             reparameterization_type=tfp.distributions.NOT_REPARAMETERIZED,
             validate_args=validate_args,
             allow_nan_stats=allow_nan_stats,
             parameters = dict(locals()),
             name=name
-        ) # TODO: Values for other args
+        )
         self.pop_size = pop_size
-        self.node_mask = node_mask
+        self.sampling_times = sampling_times
+        self.taxon_count = self.sampling_times.shape[-1]
 
-    def _log_prob(self, heights):
+    def _log_prob(self, x):
+        # TODO: Validate topology
+        node_heights = x['heights']
+        heights = tf.concat([self.sampling_times, node_heights], 0)
+        node_mask = tf.concat([tf.fill([self.taxon_count], False), tf.fill([self.taxon_count - 1], True)], 0)
+
         sort_indices = tf.argsort(heights)
         heights_sorted = tf.gather(heights, sort_indices)
-        node_mask_sorted = tf.gather(self.node_mask, sort_indices)
+        node_mask_sorted = tf.gather(node_mask, sort_indices)
 
         lineage_count =  get_lineage_count(tf.where(node_mask_sorted, COALESCENCE, SAMPLING))[:-1]
-        population_func = tf.broadcast_to(tf.expand_dims(self.pop_size, 0), lineage_count.shape) # To broadcast
+        population_func = tf.broadcast_to(tf.expand_dims(self.pop_size, 0), lineage_count.shape)
         durations = heights_sorted[1:] - heights_sorted[:-1]
         population_areas = durations / self.pop_size
         coalescent_mask = node_mask_sorted[1:]
 
         return coalescent_likelihood(lineage_count, population_func, population_areas, coalescent_mask)
+
+    def _sample_n(self, n, seed=None):
+        raise NotImplementedError('Coalescent simulator not yet implemented')
