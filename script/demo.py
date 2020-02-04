3c7d81050d63779e6e179e1786e55198e1d4bb1e
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2020-02-04 17:29:17 +1300

    Eager training

--- script/demo.py
+++ script/demo.py
@@ -10,14 +10,7 @@ def run_demo():
 
     log_prob = treeflow.model.construct_model_likelihood(newick_file, fasta_file)
     q = treeflow.model.construct_surrogate_posterior(newick_file)
-    def kl():
-        q_samples = q.sample()
-        return q.log_prob(q_samples) - log_prob(**q_samples)
-
-    with tf.GradientTape(watch_accessed_variables=True) as t:
-        loss = kl()
-
-    res = t.gradient(loss, t.watched_variables())
+    res = treeflow.vi.fit_surrogate_posterior(log_prob, q, tf.optimizers.Adam(), 10)
     print(res)
 
 
--- treeflow/vi.py
+++ treeflow/vi.py
@@ -2,26 +2,44 @@ from tensorflow_probability.python import math as tfp_math
 from tensorflow_probability.python.vi import csiszar_divergence
 
 from tensorflow_probability.python.internal import nest_util
+import numpy as np
+import tensorflow as tf
+import tqdm
 
 _trace_loss = lambda loss, grads, variables: loss
+
+def minimize_eager(loss_fn, num_steps, optimizer, trainable_variables=None, trace_fn=_trace_loss, name='minimize', iter=range):
+    state = { 'loss': np.zeros(num_steps), 'vars': {} }
+    for i in iter(num_steps):
+        with tf.GradientTape(watch_accessed_variables=trainable_variables is None) as tape:
+            for v in trainable_variables or []:
+                tape.watch(v)
+            loss = loss_fn()
+        watched_variables = tape.watched_variables()
+
+        if i == 0:
+            for variable in watched_variables:
+                state['vars'][variable.name] = np.zeros((num_steps + variable.shape).as_list())
+        state['loss'][i] = loss.numpy()
+        for variable in watched_variables:
+                state['vars'][variable.name][i] = variable.numpy()
+
+        grads = tape.gradient(loss, watched_variables)
+        optimizer.apply_gradients(zip(grads, watched_variables))
+    return state
 
 def fit_surrogate_posterior(
     target_log_prob_fn,
     surrogate_posterior,
     optimizer,
     num_steps,
-    trace_fn=_trace_loss,
-    trainable_variables=None,
     seed=None,
-    name='fit_surrogate_posterior'):
+    **min_kwargs):
     def kl():
         q_samples = surrogate_posterior.sample(seed=seed)
         return surrogate_posterior.log_prob(q_samples) - nest_util.call_fn(target_log_prob_fn, q_samples)
 
-    return tfp_math.minimize(kl,
+    return minimize_eager(kl,
         num_steps=num_steps,
-        optimizer=optimizer,
-		trace_fn=trace_fn,
-		trainable_variables=trainable_variables,
-		name=name) 
+        optimizer=optimizer, **min_kwargs) 
     
