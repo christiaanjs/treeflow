7b92ef3f6cbf73fa7dc2feb892fb3bd4379a882a
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2020-11-24 14:51:46 +1300

    Implement new scaled approximation for scalar variables

--- test/test_clock_approx.py
+++ test/test_clock_approx.py
@@ -12,24 +12,44 @@ lognormal_kwargs = dict(
 lognormal_dist = tfp.distributions.LogNormal(**lognormal_kwargs)
 clock_rate = tf.convert_to_tensor(0.1, dtype=treeflow.DEFAULT_FLOAT_DTYPE_TF)
 
-def test_clock_approx_sample_scalar(hello_newick_file):
+@pytest.fixture()
+def hello_tensor_tree_and_count(hello_newick_file):
     tree, taxon_names = treeflow.tree_processing.parse_newick(hello_newick_file)
-    tree = treeflow.tree_processing.tree_to_tensor(tree)
-    taxon_count = len(taxon_names)
-    rate_dist = treeflow.clock_approx.ScaledRateDistribution(tfp.distributions.Sample(lognormal_dist, 2*taxon_count - 2), tree, clock_rate)
-    rates = rate_dist.sample()
-    assert rates.numpy().shape == (2*taxon_count - 2,)
+    return treeflow.tree_processing.tree_to_tensor(tree), len(taxon_names)
 
-def test_clock_approx_sample_vector(hello_newick_file):
-    tree, taxon_names = treeflow.tree_processing.parse_newick(hello_newick_file)
+@pytest.fixture()
+def hello_tensor_tree_vec_and_count(hello_tensor_tree_and_count):
+    tree, taxon_count = hello_tensor_tree_and_count
     sample_shape = 2
-    tree = treeflow.tree_processing.tree_to_tensor(tree)
-    tree = dict(
+    return dict(
         topology=dict(parent_indices=tf.broadcast_to(tree['topology']['parent_indices'], [sample_shape, tree['topology']['parent_indices'].shape[0]])),
         heights=tf.broadcast_to(tree['heights'], [sample_shape, tree['heights'].shape[0]])
-    )
-    clock_vec = tf.broadcast_to(clock_rate, [sample_shape])
-    taxon_count = len(taxon_names)
+    ), taxon_count
+
+def test_approx_sample_scalar(hello_tensor_tree_and_count):
+    tree, _ = hello_tensor_tree_and_count
+    dist = treeflow.clock_approx.ScaledDistribution(lognormal_dist, tree)
+    sample = dist.sample()
+    assert sample.numpy().shape == ()
+
+def test_approx_sample_vector(hello_tensor_tree_vec_and_count):
+    tree, taxon_count = hello_tensor_tree_vec_and_count
+    base_dist = lambda **x: tfp.distributions.LogNormal(**x)
+    dist = treeflow.clock_approx.ScaledDistribution(base_dist, tree, **lognormal_kwargs)
+    sample = dist.sample()
+    sample_shape = tf.shape(tree["heights"])[:-1]
+    assert sample.numpy().shape == sample_shape
+
+def test_rate_approx_sample_scalar(hello_tensor_tree_and_count):
+    tree, taxon_count = hello_tensor_tree_and_count
+    dist = treeflow.clock_approx.ScaledRateDistribution(tfp.distributions.Sample(lognormal_dist, 2*taxon_count - 2), tree, clock_rate)
+    sample = dist.sample()
+    assert sample.numpy().shape == (2*taxon_count - 2,)
+
+def test_rate_approx_sample_vector(hello_tensor_tree_vec_and_count):
+    tree, taxon_count = hello_tensor_tree_vec_and_count
+    sample_shape = tf.shape(tree["heights"])[:-1]
+    clock_vec = tf.broadcast_to(clock_rate, sample_shape)
     base_rate_dist = lambda **x: tfp.distributions.Sample(tfp.distributions.LogNormal(**x), 2*taxon_count - 2)
     rate_dist = treeflow.clock_approx.ScaledRateDistribution(base_rate_dist, tree, clock_vec, **lognormal_kwargs)
     rates = rate_dist.sample()
--- treeflow/clock_approx.py
+++ treeflow/clock_approx.py
@@ -3,6 +3,26 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import treeflow.sequences
 
+class ScaledDistribution(tfp.distributions.TransformedDistribution):
+    def __init__(self, distribution, tree, tree_statistic="length", **dist_kwargs):
+        if tree_statistic == "length":
+            blens = treeflow.sequences.get_branch_lengths(tree)
+            statistic = tf.reduce_sum(blens, axis=-1)
+        elif tree_statistic == "height":
+            statistic = tree["heights"][..., -1]
+        
+        batch_shape = tf.shape(statistic)
+        bij = tfp.bijectors.Scale(1.0 / statistic)
+
+        if callable(distribution):
+            dist_kwargs_b = {}
+            for key, val in dist_kwargs.items():
+                arg_shape = tf.concat([batch_shape, tf.shape(val)], 0)
+                dist_kwargs_b[key] = tf.broadcast_to(val, arg_shape)
+            distribution = distribution(**dist_kwargs_b)
+
+        super(ScaledDistribution, self).__init__(distribution=distribution, bijector=bij)
+
 class ScaledRateDistribution(tfp.distributions.TransformedDistribution):
     def __init__(self, distribution, tree, clock_rate, **dist_kwargs): # TODO: dist_kwargs_event_ndims for different kwrags
         blens = treeflow.sequences.get_branch_lengths(tree)
