38920076710e307148e2fea31d364605e2d8dd36
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2020-11-25 23:03:37 +1300

    Set Beagle paths in setup

--- setup.sh
+++ setup.sh
@@ -13,16 +13,26 @@ cd beagle-lib
 ./configure --prefix=$TREEFLOW_LIB
 make install
 
-# Install libsbn
+# Set up libsbn environment
 cd $TREEFLOW_LIB
 git clone -b 264-ratio-gradient-jacobian --single-branch https://github.com/phylovi/libsbn.git
 cd libsbn
 conda env create -f environment.yml
 conda activate libsbn
 git submodule update --init --recursive
-export BEAGLE_PREFIX=$TREEFLOW_LIB
-scons
+
+# Add Beagle to libsbn environment
+BEAGLE_PREFIX=$TREEFLOW_LIB
+ACTIVATE_DIR=$CONDA_PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$CONDA_PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR $DEACTIVATE_DIR
+echo "export BEAGLE_PREFIX=$BEAGLE_PREFIX" >> $ACTIVATE_DIR/vars.sh
+echo 'export LD_LIBRARY_PATH=$BEAGLE_PREFIX/lib:$LD_LIBRARY_PATH' >> $ACTIVATE_DIR/vars.sh
+echo 'export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | perl -pe "s[$BEAGLE_PREFIX][]g;")' >> $DEACTIVATE_DIR/vars.sh
+echo "unset BEAGLE_PREFIX" >> $DEACTIVATE_DIR/vars.sh
+# Install libsbn
 conda activate libsbn
+scons
 make
 
 cd $TREEFLOW_DIR
