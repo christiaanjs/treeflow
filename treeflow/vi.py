8376b9b5e670a61f7932f33f59141a3acbeb0fea
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2021-06-30 18:01:58 +1200

    Convergence criteria for debugging

--- treeflow/vi.py
+++ treeflow/vi.py
@@ -1,3 +1,4 @@
+import tensorflow_probability as tfp
 from tensorflow_probability.python import math as tfp_math
 from tensorflow_probability.python.vi import csiszar_divergence
 
@@ -54,3 +55,27 @@ def fit_surrogate_posterior(
         )
 
     return minimize_eager(kl, num_steps=num_steps, optimizer=optimizer, **min_kwargs)
+
+
+def _any_nonfinite(x):
+    nonfinite = tf.logical_not(tf.math.is_finite(x))
+    return tf.reduce_any(nonfinite)
+
+
+class NonfiniteConvergenceCriterion(
+    tfp.optimizer.convergence_criteria.ConvergenceCriterion
+):
+    def __init__(self, name="NonfiniteConvergenceCriterion"):
+        super().__init__(min_num_steps=0, name=name)
+
+    def _bootstrap(self, loss, grads, parameters):
+        return None
+
+    def _one_step(self, step, loss, grads, parameters, auxiliary_state):
+        loss_nonfinite = _any_nonfinite(loss)
+        grads_nonfinite = [_any_nonfinite(x) for x in grads]
+        parameters_nonfinite = [_any_nonfinite(x) for x in parameters]
+        has_converged = tf.reduce_any(
+            [loss_nonfinite] + grads_nonfinite + parameters_nonfinite
+        )
+        return has_converged, None
