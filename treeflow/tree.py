93513b715dd285f19a3ab12b8a2c17c7cb887606
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2021-08-24 15:31:26 +1200

    Add missing arg to TreeDistribution._call_sample_n

--- treeflow/tree.py
+++ treeflow/tree.py
@@ -37,29 +37,31 @@ class TreeDistribution(tfp.distributions.Distribution):
         with self._name_and_control_scope(name):
             return self._log_prob(value)
 
-    def _call_sample_n(self, sample_shape, seed, **kwargs):
-        sample_shape = tf.cast(sample_shape, tf.int32, name="sample_shape")
-        sample_shape, n = self._expand_sample_shape_to_vector(
-            sample_shape, "sample_shape"
-        )
-        tree_samples = self._sample_n(
-            n,
-            seed=seed() if callable(seed) else seed,
-            **kwargs,
-        )
+    def _call_sample_n(self, sample_shape, seed, name, **kwargs):
 
-        def reshape_samples(samples):
-            batch_event_shape = tf.shape(samples)[1:]
-            final_shape = tf.concat([sample_shape, batch_event_shape], 0)
-            samples = tf.reshape(samples, final_shape)
-            # samples = self._set_sample_static_shape(samples, sample_shape)
-            return samples
+        with self._name_and_control_scope(name):
+            sample_shape = tf.cast(sample_shape, tf.int32, name="sample_shape")
+            sample_shape, n = self._expand_sample_shape_to_vector(
+                sample_shape, "sample_shape"
+            )
+            tree_samples = self._sample_n(
+                n,
+                seed=seed() if callable(seed) else seed,
+                **kwargs,
+            )
 
-        return {
-            "heights": reshape_samples(tree_samples["heights"]),
-            "topology": {
-                "parent_indices": reshape_samples(
-                    tree_samples["topology"]["parent_indices"]
-                )
-            },
-        }
+            def reshape_samples(samples):
+                batch_event_shape = tf.shape(samples)[1:]
+                final_shape = tf.concat([sample_shape, batch_event_shape], 0)
+                samples = tf.reshape(samples, final_shape)
+                # samples = self._set_sample_static_shape(samples, sample_shape)
+                return samples
+
+            return {
+                "heights": reshape_samples(tree_samples["heights"]),
+                "topology": {
+                    "parent_indices": reshape_samples(
+                        tree_samples["topology"]["parent_indices"]
+                    )
+                },
+            }
