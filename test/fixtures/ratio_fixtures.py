d11cdb46e7597366544435b99ebb22d3e89e4fe1
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2021-11-17 16:58:11 +1300

    Ratio tests passing

--- test/fixtures/ratio_fixtures.py
+++ test/fixtures/ratio_fixtures.py
@@ -1,24 +1,54 @@
+from _pytest.config import parse_warning_filter
 import pytest
+import numpy as np
 from collections import namedtuple
 from treeflow import DEFAULT_FLOAT_DTYPE_NP
 
 RatioTestData = namedtuple(
-    "RatioTestData", ["heights", "parent_indices", "preorder_indices", "ratios"]
+    "RatioTestData",
+    [
+        "heights",
+        "node_parent_indices",
+        "preorder_node_indices",
+        "ratios",
+        "anchor_heights",
+    ],
 )
 
 
 # parent indices: [5, 5, 6, 6, 8, 7, 7, 8]
 # sampling times: [0.1, 0.2, 0.0, 0.3, 0.2]
-anchor_heights = [[0.0, 0.0, 0.0, 0.0], [0.2, 0.3, 0.3, 0.3]]
-heights = [[0.2, 0.5, 0.8, 1.6], [0.6, 0.75, 1.2, 1.5]]
-ratios = [
-    [0.25, 0.625, 0.5, 1.6],
-    [0.4, 0.5, 0.75, 1.2],
+anchor_heights_flat = [
+    np.array(x) for x in [[0.0, 0.0, 0.0, 0.0], [0.2, 0.3, 0.3, 0.3]]
 ]
-node_parent_indices = [7, 7, 8]
-preorder_node_indices = [8, 7, 5, 6]
+anchor_heights = anchor_heights_flat + [np.stack(anchor_heights_flat)]
+heights_flat = [np.array(x) for x in [[0.2, 0.5, 0.8, 1.6], [0.6, 0.75, 1.2, 1.5]]]
+heights = heights_flat + [np.stack(heights_flat)]
+ratios_flat = [
+    np.array(x)
+    for x in [
+        [0.25, 0.625, 0.5, 1.6],
+        [0.4, 0.5, 0.75, 1.2],
+    ]
+]
+ratios = ratios_flat + [np.stack(ratios_flat)]
+node_parent_indices = np.array([7, 7, 8]) - 5
+preorder_node_indices = np.array([8, 7, 5, 6]) - 5
 
 
-@pytest.fixture(params=[])
+@pytest.fixture(
+    params=[
+        RatioTestData(
+            heights=heights_element,
+            node_parent_indices=node_parent_indices,
+            preorder_node_indices=preorder_node_indices,
+            ratios=ratios_element,
+            anchor_heights=anchor_heights_element,
+        )
+        for heights_element, ratios_element, anchor_heights_element in zip(
+            heights, ratios, anchor_heights
+        )
+    ]
+)
 def ratio_test_data(request):
     return request.param
--- treeflow/traversal/ratio_transform.py
+++ treeflow/traversal/ratio_transform.py
@@ -2,27 +2,36 @@ import tensorflow as tf
 from tensorflow_probability.python.internal import prefer_static as ps
 
 
+def move_outside_axis_to_inside(x):
+    rank = tf.rank(x)
+    axes = tf.range(rank)
+    perm = tf.concat([tf.range(1, rank), [0]], axis=0)
+    return tf.transpose(x, perm)
+
+
 def ratios_to_node_heights(
     preorder_node_indices: tf.Tensor,
     parent_indices: tf.Tensor,
-    proportions: tf.Tensor,
+    ratios: tf.Tensor,
     anchor_heights: tf.Tensor,
 ):
-    node_count = proportions.shape[-1]
+    node_count = ratios.shape[-1]
     node_heights_ta = tf.TensorArray(
-        dtype=proportions.dtype,
+        dtype=ratios.dtype,
         size=node_count,
-        element_shape=proportions.shape[:-1],
+        element_shape=ratios.shape[:-1],
+        clear_after_read=False,
     )
 
-    node_heights_ta.write(node_count - 1, proportions[..., node_count - 1])
+    node_heights_ta.write(
+        node_count - 1,
+        ratios[..., node_count - 1] + anchor_heights[..., node_count - 1],
+    )
     for i in preorder_node_indices[1:]:
-        parent_height = node_heights_ta.gather(
-            tf.expand_dims(parent_indices[..., i], 0)
-        )[..., 0]
+        parent_height = node_heights_ta.read(parent_indices[i])
         anchor_height = anchor_heights[..., i]
-        proportion = proportions[..., i]
+        proportion = ratios[..., i]
         node_height = (parent_height - anchor_height) * proportion + anchor_height
         node_heights_ta.write(i, node_height)
 
-    return node_heights_ta.stack()
+    return move_outside_axis_to_inside(node_heights_ta.stack())
