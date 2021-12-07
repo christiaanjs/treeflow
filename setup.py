cdbf99c9649e75ad54a7e126b189673e9ed8d83c
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2021-12-07 13:49:45 +1300

    Add license and setup config

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name="treeflow",
-    version="0.0.1",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "biopython==1.76",
-        "ete3",
-        "numdifftools",  # Test dependency
-        "pytest",
-    ],
-    extras_require=dict(accelerated=["bito"]),
-    entry_points="""
-        [console_scripts]
-        treeflow_benchmark=treeflow.cli.benchmark:treeflow_benchmark
-    """,
-)
+if __name__ == "__main__":
+    setup()
