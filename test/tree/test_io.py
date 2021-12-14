091daf8babd4cc9a29fbb9f74417f1c6e2a00261
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2021-12-14 21:07:58 +1300

    Add zero edge removal

--- test/tree/test_io.py
+++ test/tree/test_io.py
@@ -1,9 +1,12 @@
+from treeflow.tree.rooted.numpy_rooted_tree import NumpyRootedTree
 from treeflow.tree.taxon_set import TupleTaxonSet
 import pytest
 from pathlib import Path
-from treeflow.tree.io import parse_newick
+from treeflow.tree.io import parse_newick, remove_zero_edges
 import numpy as np
 from numpy.testing import assert_allclose
+
+from treeflow.tree.topology.numpy_tree_topology import NumpyTreeTopology
 
 data_dir = Path("test/data")
 
@@ -22,3 +25,25 @@ def test_parse_newick(hello_newick_file: str):
     assert tree.taxon_set == TupleTaxonSet(expected_taxon_names)
     assert_allclose(tree.heights, expected_heights, atol=1e-16)
     assert_allclose(tree.topology.parent_indices, expected_parent_indices)
+
+
+def test_remove_zero_edges_no_changes(hello_newick_file: str):
+    tree = parse_newick(hello_newick_file)
+    starting_heights = np.array(tree.heights)
+    res = remove_zero_edges(tree)
+    assert res is not tree
+    assert_allclose(tree.heights, starting_heights)
+    assert_allclose(res.heights, tree.heights)  # No zero branches
+
+
+def test_remove_zero_edges():
+    node_height = 0.5
+    epsilon = 0.01
+    parent_indices = np.array([4, 4, 5, 6, 5, 6])
+    heights = np.array([0.0, 0.1, 0.4, 0.2, node_height, node_height, node_height])
+    expected_heights = np.concatenate(
+        [heights[:4], [node_height, node_height + epsilon, node_height + 2 * epsilon]]
+    )
+    tree = NumpyRootedTree(heights, topology=NumpyTreeTopology(parent_indices))
+    res = remove_zero_edges(tree, epsilon=epsilon)
+    assert_allclose(res.heights, expected_heights)
--- treeflow/tree/io.py
+++ treeflow/tree/io.py
@@ -4,8 +4,28 @@ from treeflow import DEFAULT_FLOAT_DTYPE_NP
 from treeflow.tree.rooted.numpy_rooted_tree import NumpyRootedTree
 from treeflow.tree.taxon_set import DictTaxonSet
 
+_EPSILON = 1e-6
 
-def parse_newick(newick_file: str) -> NumpyRootedTree:
+
+def remove_zero_edges(
+    tree: NumpyRootedTree, epsilon: float = _EPSILON
+) -> NumpyRootedTree:
+    heights = np.array(tree.heights)
+    child_indices = tree.topology.child_indices
+    for node_index in tree.topology.postorder_node_indices:
+        child_heights = heights[child_indices[node_index]]
+        heights[node_index] = np.max(
+            (heights[node_index], np.max(child_heights) + epsilon)
+        )
+    return NumpyRootedTree(heights=heights, topology=tree.topology)
+
+
+_remove_zero_edges_func = remove_zero_edges
+
+
+def parse_newick(
+    newick_file: str, remove_zero_edges: bool = True, epsilon: float = _EPSILON
+) -> NumpyRootedTree:
     """Return leaves followed by nodes (postorder)"""
     t = ete3.Tree(newick_file)
     ordered_nodes = sorted(t.traverse("postorder"), key=lambda n: not n.is_leaf())
@@ -21,9 +41,13 @@ def parse_newick(newick_file: str) -> NumpyRootedTree:
 
     taxon_count = (len(ordered_nodes) + 1) // 2
     taxon_set = DictTaxonSet([x.name for x in ordered_nodes[:taxon_count]])
-    return NumpyRootedTree(
+    tree = NumpyRootedTree(
         heights=heights, parent_indices=parent_indices, taxon_set=taxon_set
     )
+    if remove_zero_edges:
+        tree = _remove_zero_edges_func(tree, epsilon=epsilon)
+
+    return tree
 
 
-__all__ = [parse_newick.__name__]
+__all__ = [parse_newick.__name__, remove_zero_edges.__name__]
