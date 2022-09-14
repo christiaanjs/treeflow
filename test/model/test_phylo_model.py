Fix broadcasting bug in SampleWeighted Closes #60

test/model/test_phylo_model.py
@@ -1,3 +1,4 @@
+import typing as tp
 import pytest
 import numpy as np
 from numpy.testing import assert_allclose
@@ -11,8 +12,12 @@ from treeflow.model.phylo_model import (
     get_site_rate_distribution,
     get_sequence_distribution,
 )
-from treeflow.tree.rooted.tensorflow_rooted_tree import TensorflowRootedTree
+from treeflow.tree.rooted.tensorflow_rooted_tree import (
+    TensorflowRootedTree,
+    convert_tree_to_tensor,
+)
 from treeflow.evolution.substitution.nucleotide.hky import HKY
+from treeflow.tree.io import parse_newick
 
 model_dicts_and_keys = [
     (
@@ -170,30 +175,31 @@ def test_phylo_model_to_joint_distribution_site_rate_variation(
 
 
 def test_phylo_model_get_sequence_distribution_vec(
-    hello_alignment: Alignment, hello_tensor_tree: TensorflowRootedTree
+    newick_fasta_file_dated: tp.Tuple[str, str, bool]
 ):
+    newick_file, fasta_file, dated = newick_fasta_file_dated
+    tree = convert_tree_to_tensor(parse_newick(newick_file))
+    alignment = Alignment(fasta_file).get_compressed_alignment()
     subst_model = HKY()
-    encoded_sequences = hello_alignment.get_encoded_sequence_tensor(
-        hello_tensor_tree.taxon_set
-    )
+    encoded_sequences = alignment.get_encoded_sequence_tensor(tree.taxon_set)
+    pattern_counts = alignment.get_weights_tensor()
     kappa = tf.constant([1.8, 2.3], dtype=DEFAULT_FLOAT_DTYPE_TF)
     frequencies = tf.constant(
         [[0.26, 0.22, 0.27, 0.25], [0.23, 0.25, 0.28, 0.24]],
         dtype=DEFAULT_FLOAT_DTYPE_TF,
     )
-    node_heights = tf.stack(
-        [hello_tensor_tree.node_heights, hello_tensor_tree.node_heights + 0.02]
-    )
+    node_heights = tf.stack([tree.node_heights, tree.node_heights + 0.02])
     site_gamma_shape = tf.constant([1.1, 1.3], dtype=DEFAULT_FLOAT_DTYPE_TF)
     clock_rate = tf.constant(1.0, dtype=DEFAULT_FLOAT_DTYPE_TF)
     batch_dist = get_sequence_distribution(
-        hello_alignment,
-        hello_tensor_tree.with_node_heights(node_heights),
+        alignment,
+        tree.with_node_heights(node_heights),
         subst_model,
         dict(kappa=kappa, frequencies=frequencies),
         "discrete_gamma",
         dict(site_gamma_shape=site_gamma_shape, category_count=4),
         clock_rate,
+        pattern_counts=pattern_counts,
     )
     batch_log_probs = batch_dist.log_prob(encoded_sequences).numpy()
 
@@ -201,52 +207,57 @@ def test_phylo_model_get_sequence_distribution_vec(
     log_probs = np.zeros(batch_size)
     for i in range(batch_size):
         dist = get_sequence_distribution(
-            hello_alignment,
-            hello_tensor_tree.with_node_heights(node_heights[i]),
+            alignment,
+            tree.with_node_heights(node_heights[i]),
             subst_model,
             dict(kappa=kappa[i], frequencies=frequencies[i]),
             "discrete_gamma",
             dict(site_gamma_shape=site_gamma_shape[i], category_count=4),
             clock_rate,
+            pattern_counts=pattern_counts,
         )
         log_probs[i] = dist.log_prob(encoded_sequences).numpy()
     assert_allclose(batch_log_probs, log_probs)
 
 
 def test_phylo_model_get_sequence_distribution_vec_singleton(
-    hello_alignment: Alignment, hello_tensor_tree: TensorflowRootedTree
+    newick_fasta_file_dated: tp.Tuple[str, str, bool]
 ):
+    newick_file, fasta_file, dated = newick_fasta_file_dated
+    tree = convert_tree_to_tensor(parse_newick(newick_file))
+    alignment = Alignment(fasta_file).get_compressed_alignment()
     subst_model = HKY()
-    encoded_sequences = hello_alignment.get_encoded_sequence_tensor(
-        hello_tensor_tree.taxon_set
-    )
+    encoded_sequences = alignment.get_encoded_sequence_tensor(tree.taxon_set)
+    pattern_counts = alignment.get_weights_tensor()
     kappa = tf.constant([1.8], dtype=DEFAULT_FLOAT_DTYPE_TF)
     frequencies = tf.constant(
         [[0.26, 0.22, 0.27, 0.25]],
         dtype=DEFAULT_FLOAT_DTYPE_TF,
     )
-    node_heights = tf.expand_dims(hello_tensor_tree.node_heights, axis=0)
+    node_heights = tf.expand_dims(tree.node_heights, axis=0)
     site_gamma_shape = tf.constant([1.3], dtype=DEFAULT_FLOAT_DTYPE_TF)
     clock_rate = tf.constant(1.0, dtype=DEFAULT_FLOAT_DTYPE_TF)
     batch_dist = get_sequence_distribution(
-        hello_alignment,
-        hello_tensor_tree.with_node_heights(node_heights),
+        alignment,
+        tree.with_node_heights(node_heights),
         subst_model,
         dict(kappa=kappa, frequencies=frequencies),
         "discrete_gamma",
         dict(site_gamma_shape=site_gamma_shape, category_count=4),
         clock_rate,
+        pattern_counts=pattern_counts,
     )
     batch_log_probs = batch_dist.log_prob(encoded_sequences).numpy()
     assert tuple(batch_log_probs.shape) == (1,)
     dist = get_sequence_distribution(
-        hello_alignment,
-        hello_tensor_tree.with_node_heights(node_heights[0]),
+        alignment,
+        tree.with_node_heights(node_heights[0]),
         subst_model,
         dict(kappa=kappa[0], frequencies=frequencies[0]),
         "discrete_gamma",
         dict(site_gamma_shape=site_gamma_shape[0], category_count=4),
         clock_rate,
+        pattern_counts=pattern_counts,
     )
     log_prob = dist.log_prob(encoded_sequences).numpy()
     assert_allclose(batch_log_probs[0], log_prob)

treeflow/distributions/sample_weighted.py
@@ -50,10 +50,16 @@ class SampleWeighted(Sample):
                 axis=0,
             ),
         )
-        lp = tf.broadcast_to(lp, bcast_lp_shape)
+        lp_b = tf.broadcast_to(lp, bcast_lp_shape)
         # (2) Make the final reduction.
         axis = ps.range(sample_ndims, sample_ndims + extra_sample_ndims)
-        return self._sum_fn()(lp * self.weights, axis=axis)
+        weights_b = tf.reshape(
+            self.weights,
+            tf.concat(
+                [tf.shape(self.weights), tf.ones(batch_ndims, dtype=tf.int32)], axis=0
+            ),
+        )
+        return self._sum_fn()(lp_b * weights_b, axis=axis)
 
     @classmethod
     def _parameter_properties(cls, dtype, num_classes=None):

treeflow/vi/fixed_topology_advi.py
@@ -41,29 +41,15 @@ def fit_fixed_topology_variational_approximation(
     else:
         augmented_trace_fn = _trace_has_converged(trace_fn, tf.reduce_all)
 
-    def loss():
-        approx_sample = approximation.sample()
-        return approximation.log_prob(approx_sample) - model.unnormalized_log_prob(
-            approx_sample
-        )
-
-    trace = minimize(
-        loss,
-        num_steps,
+    trace = fit_surrogate_posterior(
+        model.unnormalized_log_prob,
+        approximation,
         optimizer,
+        num_steps,
         convergence_criterion=convergence_criterion,
         trace_fn=augmented_trace_fn,
         **vi_kwargs,
     )
-    # fit_surrogate_posterior(
-    #     model.unnormalized_log_prob,
-    #     approximation,
-    #     optimizer,
-    #     num_steps,
-    #     convergence_criterion=convergence_criterion,
-    #     trace_fn=augmented_trace_fn,
-    #     **vi_kwargs,
-    # )
 
     if return_full_length_trace:
         opt_res = trace