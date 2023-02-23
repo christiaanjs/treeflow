b0d58c7576e544f86b2787beea259f2bc5f9c19d
christiaanjs <christiaan.j.s@gmail.com>
2023-02-23 21:36:04 +1300

    Postorder sampling working

--- treeflow/distributions/markov_chain/postorder.py
+++ treeflow/distributions/markov_chain/postorder.py
@@ -15,6 +15,9 @@ class PostorderNodeMarkovChain(Distribution):
         transition_fn: tp.Callable[[object, object], Distribution],
         dist_input: object,
         childless_init: tp.Optional[object] = None,
+        validate_args=False,
+        allow_nan_stats=True,
+        name="PostorderNodeMarkovChain",
     ):
         """
         Parameters
@@ -23,6 +26,7 @@ class PostorderNodeMarkovChain(Distribution):
 
         transition_fn
             Callable with arguments (input, child values)
+            Must get batch shape from child values
         """
         self._transition_fn = transition_fn
         self._topology = topology
@@ -39,24 +43,35 @@ class PostorderNodeMarkovChain(Distribution):
             self._dist_event_shape,
         )
         self._dist_dtype = concrete_dist.dtype
+        super().__init__(
+            self._dist_dtype,
+            concrete_dist.reparameterization_type,
+            validate_args=validate_args,
+            allow_nan_stats=allow_nan_stats,
+            name=name,
+        )
 
     def _sample_n(self, n, seed=None):
         # track seed
 
         taxon_count = self._topology.taxon_count
-        traversal_seeds = samplers.split_seed(self._topology.taxon_count)
+        traversal_seeds = samplers.split_seed(seed=seed, n=self._topology.taxon_count)
 
         def sample_mapping(child_samples, input, topology_data: PostorderTopologyData):
             node_seed, dist_input = input
             child_node_mask = topology_data.child_indices >= taxon_count
             node_child_samples = tf.nest.map_structure(
-                lambda x: x[child_node_mask], child_samples
+                lambda x: tf.ragged.boolean_mask(x, child_node_mask), child_samples
             )
-            node_dist = self._transition_fn(node_child_samples, dist_input)
-            return node_dist.sample(n, seed=node_seed)
+            node_dist = self._transition_fn(dist_input, node_child_samples)
+            return node_dist.sample(
+                seed=node_seed
+            )  # Sample shape passed in as batch shape
 
         dummy_leaf_init = tf.nest.map_structure(
-            lambda shape, dtype: tf.zeros(tf.concat([[taxon_count], shape], 0), dtype),
+            lambda shape, dtype: tf.zeros(
+                tf.concat([[taxon_count], [n], shape], 0), dtype
+            ),
             self._dist_event_shape,
             self._dist_dtype,
         )
@@ -72,7 +87,7 @@ class PostorderNodeMarkovChain(Distribution):
         )
         transposed = tf.nest.map_structure(
             lambda x, event_rank: distribution_util.move_dimension(
-                x, 0, ps.rank(x) - event_rank
+                x, 0, ps.rank(x) - event_rank - 1
             ),
             node_only_result,
             self._dist_event_rank,
