d59daebb15785db81079cc0879250ff523e2d8cd
christiaanjs <christiaan.j.s@gmail.com>
2023-03-31 20:53:15 +1300

    Tweak root functionality of preorder bijector

--- test/bijectors/test_preorder_node_bijector.py
+++ test/bijectors/test_preorder_node_bijector.py
@@ -16,37 +16,24 @@ def ratio_transform_forward_mapping(parent_height, anchor_height):
     return Chain([Shift(anchor_height), Scale(parent_height - anchor_height)])
 
 
+def ratio_transform_forward_root_mapping(anchor_height):
+    return Shift(anchor_height)
+
+
 def get_bijector(ratio_test_data: RatioTestData) -> PreorderNodeBijector:
     topology = topology_from_ratio_test_data(ratio_test_data)
-    root_heights = tf.constant(
-        ratio_test_data.heights[..., -1], dtype=DEFAULT_FLOAT_DTYPE_TF
-    )
     bijector = PreorderNodeBijector(
         topology,
-        ratio_test_data.anchor_heights[..., :-1],
+        ratio_test_data.anchor_heights,
         ratio_transform_forward_mapping,
-        root_heights,
+        ratio_transform_forward_root_mapping,
     )
     return bijector
 
 
-def get_ratios_with_root_height(ratio_test_data: RatioTestData) -> tf.Tensor:
-    root_heights = tf.constant(
-        ratio_test_data.heights[..., -1], dtype=DEFAULT_FLOAT_DTYPE_TF
-    )
-    ratios_with_root_height = tf.constant(
-        np.concatenate(
-            [ratio_test_data.ratios[..., :-1], tf.expand_dims(root_heights, -1)], -1
-        ),
-        dtype=DEFAULT_FLOAT_DTYPE_TF,
-    )
-    return ratios_with_root_height
-
-
 def test_preorder_node_bijector_forward(ratio_test_data: RatioTestData):
-    ratios_with_root_height = get_ratios_with_root_height(ratio_test_data)
     bijector = get_bijector(ratio_test_data)
-    res = bijector.forward(ratios_with_root_height)
+    res = bijector.forward(ratio_test_data.ratios)
     expected = ratio_test_data.heights
     assert_allclose(res.numpy(), expected)
 
@@ -54,9 +41,8 @@ def test_preorder_node_bijector_forward(ratio_test_data: RatioTestData):
 def test_preorder_node_bijector_forward_log_det_jacobian(
     ratio_test_data: RatioTestData,
 ):
-    ratios_with_root_height = get_ratios_with_root_height(ratio_test_data)
     bijector = get_bijector(ratio_test_data)
-    res = bijector.forward_log_det_jacobian(ratios_with_root_height)
+    res = bijector.forward_log_det_jacobian(ratio_test_data.ratios)
 
     test_bijector = NodeHeightRatioBijector(
         bijector._topology, ratio_test_data.anchor_heights
@@ -66,10 +52,7 @@ def test_preorder_node_bijector_forward_log_det_jacobian(
     assert_allclose(res.numpy(), expected.numpy())
 
 
-def test_preorder_node_bijector_inverse(
-    ratio_test_data: RatioTestData, tensor_constant
-):
-    ratios_with_root_height = get_ratios_with_root_height(ratio_test_data)
+def test_preorder_node_bijector_inverse(ratio_test_data: RatioTestData):
     bijector = get_bijector(ratio_test_data)
     res = bijector.inverse(tf.constant(ratio_test_data.heights, DEFAULT_FLOAT_DTYPE_TF))
-    assert_allclose(res.numpy(), ratios_with_root_height.numpy())
+    assert_allclose(res.numpy(), ratio_test_data.ratios)
--- treeflow/bijectors/preorder_node_bijector.py
+++ treeflow/bijectors/preorder_node_bijector.py
@@ -19,7 +19,7 @@ class PreorderNodeBijector(Bijector):
         topology: TensorflowTreeTopology,
         input: object,
         bijector_func: tp.Callable[[object, object], Bijector],
-        bijector_init: object,
+        root_bijector_func: tp.Callable[[object], Bijector],
         name=None,
         validate_args=False,
     ):
@@ -30,28 +30,27 @@ class PreorderNodeBijector(Bijector):
             Tensor or structure of Tensors with node dimension first
         bijector_func
             Callable with signature (parent_output, input_element)
-        bijector_init
-            Passed to bijector_func for root node
+        root_bijector_func
+            Callable with signature (input_element)
         """
         parameters = locals()
         self._topology = topology
         self._input_node_first = _move_node_dimension_to_beginning(input)
         self._bijector_func = bijector_func
-        self._bijector_init = bijector_init
+        self._root_bijector_func = root_bijector_func
 
-        self._concrete_bijector = self._bijector_func(
-            bijector_init,
+        self._root_bijector = self._root_bijector_func(
             tf.nest.map_structure(lambda x: x[-1], self._input_node_first),
         )
 
         super().__init__(
             parameters=parameters,
-            dtype=self._concrete_bijector.dtype,
+            dtype=self._root_bijector.dtype,
             forward_min_event_ndims=tf.nest.map_structure(
-                lambda x: x + 1, self._concrete_bijector.forward_min_event_ndims
+                lambda x: x + 1, self._root_bijector.forward_min_event_ndims
             ),
             inverse_min_event_ndims=tf.nest.map_structure(
-                lambda x: x + 1, self._concrete_bijector.inverse_min_event_ndims
+                lambda x: x + 1, self._root_bijector.inverse_min_event_ndims
             ),
             validate_args=validate_args,
             name=name,
@@ -67,13 +66,17 @@ class PreorderNodeBijector(Bijector):
             self._topology,
             self._forward_mapping,
             (self._input_node_first, x_node_first),
-            tf.nest.map_structure(lambda x: x[-1], x_node_first),
+            self._root_bijector.forward(
+                tf.nest.map_structure(lambda x: x[-1], x_node_first)
+            ),
         )
         return tf.nest.map_structure(
             lambda x: distribution_util.move_dimension(x, 0, -1), res
         )
 
-    def _inverse_bijectors_and_y_node_first(self, y) -> tp.Tuple[Bijector, object]:
+    def _inverse_bijectors_and_y_node_first(
+        self, y
+    ) -> tp.Tuple[Bijector, object, object]:
         y_node_first = _move_node_dimension_to_beginning(y)
         parent_values = tf.nest.map_structure(
             lambda y_elem: tf.gather(
@@ -83,25 +86,40 @@ class PreorderNodeBijector(Bijector):
             ),
             y_node_first,
         )
-        bijectors = self._bijector_func(parent_values, self._input_node_first)
-        return bijectors, y_node_first
+        nonroot_input = tf.nest.map_structure(lambda x: x[:-1], self._input_node_first)
+        bijectors = self._bijector_func(parent_values, nonroot_input)
+        nonroot_values = tf.nest.map_structure(lambda y: y[:-1], y_node_first)
+        root_values = tf.nest.map_structure(lambda y: y[-1], y_node_first)
+        return bijectors, nonroot_values, root_values
 
     def _inverse_log_det_jacobian(self, y):
-        bijectors, y_node_first = self._inverse_bijectors_and_y_node_first(y)
-        node_values = bijectors.inverse_log_det_jacobian(y_node_first[:-1])
-        return tf.reduce_sum(node_values, 0)
+        (
+            bijectors,
+            nonroot_values,
+            root_values,
+        ) = self._inverse_bijectors_and_y_node_first(y)
+        node_values = bijectors.inverse_log_det_jacobian(nonroot_values)
+        return tf.reduce_sum(
+            node_values, 0
+        ) + self._root_bijector.inverse_log_det_jacobian(root_values)
 
     def _inverse(self, y):
-        bijectors, y_node_first = self._inverse_bijectors_and_y_node_first(y)
-        nonroot_values_node_first = bijectors.inverse(y_node_first[:-1])
-        nonroot_values = tf.nest.map_structure(
+        (
+            bijectors,
+            nonroot_y_values,
+            root_y_values,
+        ) = self._inverse_bijectors_and_y_node_first(y)
+        nonroot_x_values_node_first = bijectors.inverse(nonroot_y_values)
+        root_x_values = self._root_bijector.inverse(root_y_values)
+        nonroot_x_values = tf.nest.map_structure(
             lambda x: distribution_util.move_dimension(x, 0, -1),
-            nonroot_values_node_first,
+            nonroot_x_values_node_first,
         )
+
         return tf.nest.map_structure(
-            lambda nonroot_elem, y_elem: tf.concat(
-                [nonroot_elem, y_elem[..., -1:]], -1
+            lambda nonroot_elem, root_elem: tf.concat(
+                [nonroot_elem, tf.expand_dims(root_elem, -1)], -1
             ),
-            nonroot_values,
-            y,
+            nonroot_x_values,
+            root_x_values,
         )
