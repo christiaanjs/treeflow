b98b03a4fdf1809724fe67b9453a3e5b09f7c388
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2023-06-25 20:02:37 +1200

    Some distribution docstrings

---
+++ treeflow/distributions/tree/__init__.py
@@ -0,0 +1,9 @@
+from treeflow.distributions.tree.birthdeath.birth_death_contemporary_sampling import (
+    BirthDeathContemporarySampling,
+)
+from treeflow.distributions.tree.birthdeath.yule import Yule
+from treeflow.distributions.tree.coalescent.constant_coalescent import (
+    ConstantCoalescent,
+)
+
+__all__ = ["Yule", "BirthDeathContemporarySampling", "ConstantCoalescent"]
